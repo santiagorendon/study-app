17a8650a78c590bd03d0907b1c0262ad5e7d2e8a
Andre <vtavares.andre@gmail.com>
2021-04-10 14:22:04 -0300

    study room v1

--- src/client/components/Pages/StudyRoom/UserList.jsx
+++ src/client/components/Pages/StudyRoom/UserList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Grid, Typography , Container, Box} from '@material-ui/core'
+import { Grid, Typography, Container, Box } from '@material-ui/core'
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
@@ -12,7 +12,11 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
     root: {
         paddingTop: '1rem',
-        // background: '#E0E5EB',
+        background: '#C4C4C4',
+        minHeight: '26rem',
+        borderRadius: '5px',
+        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.14), 0px 3px 4px rgba(0, 0, 0, 0.12), 0px 1px 5px rgba(0, 0, 0, 0.2)',
+    
     },
 
 
@@ -23,14 +27,14 @@ const UserList = () => {
 
     function generate(element) {
         return [0, 1, 2].map((value) =>
-          React.cloneElement(element, {
-            key: value,
-          }),
+            React.cloneElement(element, {
+                key: value,
+            }),
         );
-      }
+    }
     return (
         <Container className={classes.root}>
-            <Typography  align='center' gutterBottom variant="h5" >
+            <Typography align='center' gutterBottom variant="h5" >
                 User List
             </Typography>
             <div >
--- src/client/components/Pages/StudyRoom/index.jsx
+++ src/client/components/Pages/StudyRoom/index.jsx
@@ -1,32 +1,34 @@
 import React from 'react'
-import { Grid, Typography , Container, Box} from '@material-ui/core'
+import { Grid, Typography, Container, Box } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
 
 import TextField from '@material-ui/core/TextField';
 import UserList from './UserList';
 
 const useStyles = makeStyles({
     root: {
-
+        
         background: '#E0E5EB',
     },
-    userList: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
+    wrap: {
+        
+
+        padding:'2rem',
     },
+    videoContainer: {
+        // overflow: 'hidden',
+        // position: 'relative',
+        // maxWidth: '500px',
+    },
+
     chat: {
         background: '#888',
+        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.14), 0px 3px 4px rgba(0, 0, 0, 0.12), 0px 1px 5px rgba(0, 0, 0, 0.2)',
     },
     form: {
         background: '#999',
@@ -37,63 +39,84 @@ const useStyles = makeStyles({
 });
 function generate(element) {
     return [0, 1, 2].map((value) =>
-      React.cloneElement(element, {
-        key: value,
-      }),
+        React.cloneElement(element, {
+            key: value,
+        }),
     );
-  }
+}
 
 const StudyRoom = () => {
     const classes = useStyles();
 
     return (
         <Container className={classes.root}>
-            <Grid container>
-                <Grid className='box' item sm={7}>
+            <Grid container className={classes.wrap}>
+                <Grid className='boxx' item sm={7} xs={12}>
                     <Typography variant="h2" component="h1">
                         Study Room Name
                     </Typography>
                 </Grid>
-                <Grid className='box' item sm={1}>
+                <Grid className='boxx' item xs sm={1}>
+                    <Box m={1}>
+
                     Date
+                    </Box>
                 </Grid>
-                <Grid className='box'
+                <Grid
+                    sm={4}
+                    xs={6}
+                    className='boxx'
                     container
                     direction="column"
                     justify="space-evenly"
                     alignItems="center"
-                    item sm={4}>
-                    <Button variant="contained">Default</Button>
-                    <Button variant="contained" color="primary"> Button</Button>
+                    item >
+                    <Box m={1}>
+                        <Button variant="contained">Leave Room</Button>
+                    </Box>
+                    <Box m={1} mb={2}>
+                        <Button variant="contained" color="primary"> Share room</Button>
+                    </Box>
                 </Grid>
-                <Grid container
+                {/* row 2 */}
+                <Grid
+                    xs={12}
+                    sm
+                    container
                     direction="column"
                     justify="space-evenly"
-                    alignItems="center" className='box' item sm={8}>
-                    <Box p={3}>
-                        <Typography variant="body1" component="h5">
+                    alignItems="center"
+                    className='boxx'
+                    item
+                >
+                    <Box p={2}>
+
+
+                        <Typography variant="body1" component="p">
                             Bio : Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the e release of Letraset sheets containing Lorem Ipsum passages, and more recently with de
                         </Typography>
                     </Box>
-                    <Box><iframe width="560" height="315" src="https://www.youtube.com/embed/reRYtjr1BNo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </Box>
+
+                    <Box className={classes.videoContainer} ><iframe width="100%" height="300" src="https://www.youtube.com/embed/reRYtjr1BNo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe> </Box>
 
                 </Grid>
                 {/* user list */}
-                <Grid className='box'
+                <Grid
+                    xs={12}
+                    sm={4}
+                    className='boxx'
                     container
                     direction="column"
                     justify="start"
                     alignItems="center"
-                    item 
-                    sm={4}>
+                    item
+                >
 
-                        <UserList/>
-               
+                    <UserList />
 
-      
                 </Grid>
 
-                <Grid sm={12}>
+                <Grid sm={12} xs={12}>
                     <Card className={classes.chat}>
                         <CardContent>
                             <Typography color="textSecondary" gutterBottom>
@@ -125,9 +148,6 @@ const StudyRoom = () => {
 
             </Grid>
 
-            <aside>
-
-            </aside>
 
         </Container>
 
@@ -135,3 +155,4 @@ const StudyRoom = () => {
 }
 
 export default StudyRoom
+
