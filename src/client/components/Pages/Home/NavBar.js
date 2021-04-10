c8f1000aa9d3488500c01292baedda03c8ba7a37
crystal <noelani.villanueva@gmail.com>
2021-04-10 07:44:10 -0700

    finished nav bar basis so that it can be toggled from a drawer

---
+++ src/client/components/Pages/Home/NavBar.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { Button, Drawer, List, Divider, ListItem } from "@material-ui/core";
+import { Redirect, Link } from "react-router-dom";
+
+function NavBar() {
+  const [state, setState] = useState(false);
+  const navStyle = {
+    backgroundColor: "pink",
+    opacity: ".6",
+    display: "flex",
+    justifyContent: "space-around",
+  };
+
+  const buttonStyle = { position: "relative", top: "8.5px" };
+
+  const toggleDrawer = (open) => (event) => {
+    setState(open);
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+  };
+
+  const list = () => (
+    <div onClick={toggleDrawer(false)}>
+      <List>
+        <ListItem>
+          <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+            Home
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link
+            to="/profile"
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            Profile
+          </Link>
+        </ListItem>
+
+        {/* <ListItem>
+          <Link to="/group" >
+            create a group
+          </Link>
+        </ListItem> */}
+        {/* {!localStorage.token && <Redirect to="/login" />}
+        <ListItem button onClick={handleLogout}>
+          Logout
+        </ListItem> */}
+      </List>
+    </div>
+  );
+
+  return (
+    <div style={navStyle}>
+      <Button onClick={toggleDrawer(true)} style={buttonStyle}>
+        Menu
+      </Button>
+      <Drawer anchor={"top"} open={state} onClose={toggleDrawer(false)}>
+        {list()}
+      </Drawer>
+    </div>
+  );
+}
+
+export default NavBar;
--- src/client/components/Pages/Home/index.jsx
+++ src/client/components/Pages/Home/index.jsx
@@ -1,43 +1,40 @@
-
-import Axios from 'axios'
-import React, { useState, useEffect, useContext } from 'react'
-import StudyRoomCard from './StudyRoomCard'
+import Axios from "axios";
+import React, { useState, useEffect, useContext } from "react";
+import StudyRoomCard from "./StudyRoomCard";
 // import { NotificationContext } from '../../shared/Notifications';
-import { NotificationContext } from '../../shared/Notifications'
-import { Container } from '@material-ui/core'
+import { NotificationContext } from "../../shared/Notifications";
+import { Container } from "@material-ui/core";
+import NavBar from "./NavBar";
 
 const Home = () => {
-    const getRooms = "http://localhost:8080/rooms"
-    const { setNotification } = useContext(NotificationContext);
-    const [rooms, setRooms] = useState([])
+  const getRooms = "http://localhost:8080/rooms";
+  const { setNotification } = useContext(NotificationContext);
+  const [rooms, setRooms] = useState([]);
 
-    useEffect(() => {
-        Axios.get(getRooms)
-            .then(res => {
-                if (res === none) {
-                    setNotification({
-                        type: "error",
-                        message: "Not Found"
-                    });
-                } else if (res.status === 200) {
-                    setRooms(res.data)
+  useEffect(() => {
+    Axios.get(getRooms).then((res) => {
+      if (res === none) {
+        setNotification({
+          type: "error",
+          message: "Not Found",
+        });
+      } else if (res.status === 200) {
+        setRooms(res.data);
+      } else {
+        setNotification({
+          type: "error",
+          message: "Something is wrong!",
+        });
+      }
+    });
+  }, []);
 
-                } else {
-                    setNotification({
-                        type: "error",
-                        message: "Something is wrong!"
-                    });
-                }
-            })
-    }, [])
+  return (
+    <div>
+      <NavBar />
 
-    return (
-        <div>
-            <h1>  This is home</h1>
-
-
-                {/* get from, the DB and populate the room card */}
-            {/* <Container>
+      {/* get from, the DB and populate the room card */}
+      {/* <Container>
                 {rooms.map((room, i) => (
                     <StudyRoomCard
                         groupName=' Study group Name'
@@ -49,16 +46,15 @@ const Home = () => {
                 ))}
             </Container> */}
 
-            <StudyRoomCard
-                groupName=' Study group Example'
-                tags='Tags'
-                bio='Bio about the class and resources. embed the time of recurrent meeting'
-                meetDatetime='21/21'
-                userAmount='03' />
+      <StudyRoomCard
+        groupName=" Study group Example"
+        tags="Tags"
+        bio="Bio about the class and resources. embed the time of recurrent meeting"
+        meetDatetime="21/21"
+        userAmount="03"
+      />
+    </div>
+  );
+};
 
-
-        </div>
-    )
-}
-
-export default Home
+export default Home;
