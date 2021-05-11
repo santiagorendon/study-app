c3559684a2814cd29f6bdd53f140ae887798a766
Andre <vtavares.andre@gmail.com>
2021-05-11 20:42:25 -0300

    add componentization to profile

--- src/client/components/Pages/Profile/ProfileCard.jsx
+++ src/client/components/Pages/Profile/ProfileCard.jsx
@@ -1,29 +1,28 @@
 import React from "react"
 import {
-    Paper,
-    Avatar,
-    Grid,
-    Typography,
-    Card,
-    Button,
-    CardContent,
-    CardActions,
-    CardActionArea,
-    TextField,
-  } from "@material-ui/core";
+	Paper,
+	Avatar,
+	Grid,
+	Typography,
+	Card,
+	Button,
+	CardContent,
+	CardActions,
+	CardActionArea,
+	TextField,
+} from "@material-ui/core"
 
-  const ProfileCard = ({openModal, user}) => {
-    const paperStyle2 = {
-      padding: "20%",
-      height: "50vh",
-      width: "280%",
-      margin: "20px auto",
-      overflow: "scroll",
-    
-    };
+const ProfileCard = ({ openModal, user }) => {
+	const paperStyle2 = {
+		padding: "2rem",
+		margin: "2rem",
+		//   height: "50vh",
+		//   width: "280%",
+		//   overflow: "scroll",
+	}
 	return (
 		<Grid item xs>
-			<Paper elevation={20} style={paperStyle2}>
+			<Paper elevation={5} style={paperStyle2}>
 				<Grid align='center'>
 					<h3>Profile</h3>
 					<Grid>
@@ -35,8 +34,7 @@ import {
 					</div>
 
 					<Button onClick={openModal} color='secondary'>
-						{" "}
-						Edit Profile{" "}
+						Edit Profile
 					</Button>
 				</Grid>
 			</Paper>
---
+++ src/client/components/Pages/Profile/StudyGroupCard.jsx
@@ -0,0 +1,58 @@
+import React from "react"
+import {
+	Paper,
+	Avatar,
+	Grid,
+	Typography,
+	Card,
+	Button,
+	CardContent,
+	CardActions,
+	CardActionArea,
+	TextField,
+} from "@material-ui/core"
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline"
+
+const paperCardStyle = {
+	padding: "2rem",
+	// height: "50vh",
+	//   width: "280%",
+	//   margin: "20px auto",
+	//   overflow: "scroll",
+}
+
+const StudyGroupCard = ({ groups }) => {
+	return (
+		<Grid style={paperCardStyle} align='center'>
+			<Paper elevation={5}>
+				<Card variant='outlined'>
+                <h4> User Study Groups</h4>
+					<CardContent>
+						{groups
+							? groups.map((group) => (
+									<>
+										<Typography color='textSecondary' gutterBottom>
+											{group}
+										</Typography>
+										<Typography variant='h5' component='h2'>
+											{group.bio}
+										</Typography>
+									</>
+							  ))
+							: "you have no study groups"}
+
+						<CardActions>
+							<CardActionArea>
+								{/* <Button size='small' onClick={() => handleDelete(group)}>
+									<DeleteOutlineIcon></DeleteOutlineIcon>
+								</Button> */}
+							</CardActionArea>
+						</CardActions>
+					</CardContent>
+				</Card>
+			</Paper>
+		</Grid>
+	)
+}
+
+export default StudyGroupCard
