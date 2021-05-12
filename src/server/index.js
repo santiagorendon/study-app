be0e5d4a4cca29acf561269ceba341dd3e34f7a3
Santiago Rendon <sr4670@nyu.edu>
2021-05-12 19:29:43 -0400

    added socket libraries and basic connection

--- src/server/index.js
+++ src/server/index.js
@@ -13,6 +13,7 @@ app.use(express.static("dist"));
 app.use(express.urlencoded({ extended: false }));
 const session = require("express-session");
 
+
 const sessionOptions = {
   secret: process.env.SESSION_SECRET,
   saveUninitialized: false,
@@ -22,21 +23,26 @@ app.use(session(sessionOptions));
 
 app.use(function(req, res, next) {
   // Website you wish to allow to connect
-res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-// Request methods you wish to allow
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-// Request headers you wish to allow
-res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-// Set to true if you need the website to include cookies in the requests sent
-// to the API (e.g. in case you use sessions)
-res.setHeader('Access-Control-Allow-Credentials', true);
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
-// Pass to next layer of middleware
-next();
+  // Pass to next layer of middleware
+  next();
 });
+console.log("YEEE")
+// const server = require('http').Server(app);
+
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
 
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
@@ -338,4 +344,15 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+const server = app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+})
+
+io.on("connection", socket => {
+  console.log("yuh")
+})
